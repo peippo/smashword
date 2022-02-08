f893fd6ef83ce467999f5949b224add6f1c29fd4
Janne Peippo <janne.peippo@poutapilvi.fi>
2022-02-08 21:15:16 +0200

    Remove length slider

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import Logo from "./components/Logo.svelte";
 	import Instructions from "./components/Instructions.svelte";
-	import LengthSelect from "./components/LengthSelect.svelte";
 	import CharTypeSelect from "./components/CharTypeSelect.svelte";
 	import Password from "./components/Password.svelte";
 </script>
@@ -9,7 +8,6 @@
 <main>
 	<Logo />
 	<Instructions />
-	<LengthSelect />
 	<CharTypeSelect />
 	<Password />
 </main>
--- src/components/LengthSelect.svelte
+++
@@ -1,5 +0,0 @@
-<script type="ts">
-	import { maxLength } from "../store";
-</script>
-
-<input type="range" min="8" max="64" step="4" bind:value={$maxLength} />
